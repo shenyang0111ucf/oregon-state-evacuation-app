9692483a5c7a46af7c79e2dc7758707a7b59cf00
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2021-11-01 15:29:09 -0700

    made toggle button

--- evac_app/lib/app.dart
+++ evac_app/lib/app.dart
@@ -3,19 +3,60 @@ import 'package:flutter/material.dart';
 import './styles.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool? _running;
+
+  @override
+  void initState() {
+    _running = false;
+    super.initState();
+  }
+
+  void toggleRunning() {
+    // TODO: call toggle to service
+
+    setState(() {
+      _running = !_running!;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
+      title: widget.title,
       theme: Styles.darkTheme,
       home: EvacAppScaffold(
-        title: title,
-        child: Center(),
+        title: widget.title,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              runningToggleButton(context),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget runningToggleButton(BuildContext context) {
+    return ElevatedButton(
+      child: SizedBox(
+        child: Text(
+          (_running!) ? 'Stop' : 'Start',
+          textAlign: TextAlign.center,
+        ),
+        width: 60,
+      ),
+      onPressed: toggleRunning,
     );
   }
 }
