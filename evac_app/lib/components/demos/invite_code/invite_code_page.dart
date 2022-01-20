0904a9bf326e02cc8b406e408d86f80a00801407
tangdingguo <767435676@qq.com>
2022-01-20 10:44:11 -0800

    Update invite_code_page.dart

--- evac_app/lib/components/demos/invite_code/invite_code_page.dart
+++ evac_app/lib/components/demos/invite_code/invite_code_page.dart
@@ -1,25 +1,91 @@
-import 'package:evac_app/components/evac_app_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:evac_app/components/evac_app_scaffold.dart';
 
-class InviteCodePage extends StatefulWidget {
+class InviteCodePage extends StatelessWidget {
   const InviteCodePage({Key? key}) : super(key: key);
 
+  static const String _title = 'Flutter Code Sample';
+
   @override
-  _InviteCodePageState createState() => _InviteCodePageState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: InviteCodePageWidget(),
+    );
+  }
 }
 
-class _InviteCodePageState extends State<InviteCodePage> {
-  // TODO: 2: Write a function to validate input
+class InviteCodePageWidget extends StatefulWidget {
+  const InviteCodePageWidget({Key? key}) : super(key: key);
 
-  // TODO: 3: Output any valid input to Snackbar using following snippet:
-  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text()));
+  @override
+  State<InviteCodePageWidget> createState() => _InviteCodePageWidgetState();
+}
+
+class _InviteCodePageWidgetState extends State<InviteCodePageWidget> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return EvacAppScaffold(
-      title: 'invite code page',
-      // TODO: 1: create widgets that allow six digits to be input
-      child: Container(),
+      title: "invite code page",
+      child: Container(
+        child: TextField(
+          controller: _controller,
+          onSubmitted: (String value) async {
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                final codeExp = RegExp(r'^[0-9]{6}$');
+                if (codeExp.hasMatch(value)) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(value)));
+                  return AlertDialog(
+                    title: const Text('Thanks!'),
+                    content: const Text('Thank you for joining us!'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                }
+                return AlertDialog(
+                  title: const Text('Error!'),
+                  content: const Text('Invite code is 6 digital.'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Invite Code',
+          ),
+        ),
+      ),
     );
   }
 }
