68921fdee9ad912266037832b43792a90cfafd1a
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2022-02-03 19:59:23 -0800

    test actually confirms and outputs json string that can go to Firestore

--- evac_app/test/drill_event_json_test.dart
+++ evac_app/test/drill_event_json_test.dart
@@ -5,5 +5,7 @@ import 'package:evac_app/models/drill_event.dart';
 void main() {
   var drillEventJson = DrillEvent.example().toJson();
   var drillEvent = DrillEvent.fromJson(drillEventJson);
-  print(jsonEncode(drillEvent.toJson()));
+  var encoded = jsonEncode(drillEvent.toJson());
+  var newDrillEvent = DrillEvent.fromJson(jsonDecode(encoded));
+  print(jsonEncode(newDrillEvent.toJson()));
 }
