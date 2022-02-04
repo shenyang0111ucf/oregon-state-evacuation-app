feb7c98000f527f9c3f87821a7656023e61c73a4
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2022-02-04 15:41:25 -0800

    tweaking invite_code_page
    padding, remove TextEditingController()

--- evac_app/lib/pages/invite_code_page.dart
+++ evac_app/lib/pages/invite_code_page.dart
@@ -2,74 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:evac_app/components/evac_app_scaffold.dart';
 
 class InviteCodePage extends StatefulWidget {
-  const InviteCodePage({Key? key}) : super(key: key);
+  const InviteCodePage({
+    Key? key,
+    required this.tryInviteCode,
+  }) : super(key: key);
+
+  static const valueKey = ValueKey('InviteCodePage');
+  final Function tryInviteCode;
 
   @override
   State<InviteCodePage> createState() => _InviteCodePageState();
 }
 
 class _InviteCodePageState extends State<InviteCodePage> {
-  late TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return EvacAppScaffold(
       title: "invite code page",
-      child: Container(
-        child: TextField(
-          controller: _controller,
-          onSubmitted: (String value) async {
-            await showDialog<void>(
-              context: context,
-              builder: (BuildContext context) {
-                final codeExp = RegExp(r'^[0-9]{6}$');
-                if (codeExp.hasMatch(value)) {
-                  return AlertDialog(
-                    title: const Text('Thanks!'),
-                    content: Text('Thank you for joining us with code $value!'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text('OK'),
-                      ),
-                    ],
-                  );
-                }
-                return AlertDialog(
-                  title: const Text('Error!'),
-                  content: const Text('Invite code is 6 digital.'),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('OK'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Invite Code',
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 8.0,
+          right: 8.0,
+          top: 20.0,
+        ),
+        child: Container(
+          child: TextField(
+            onSubmitted: (String value) async {
+              final codeExp = RegExp(r'^[0-9]{6}$');
+              if (codeExp.hasMatch(value)) {
+                _tryCode(context, value);
+              } else {
+                await showDialog<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Error!'),
+                        content: const Text('Invite code should be 6 digits.'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      );
+                    });
+              }
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Invite Code',
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _tryCode(BuildContext context, inputCode) async {
+    var success = await widget.tryInviteCode(inputCode);
+    if (!success) {
+      // pop up error
+    }
   }
 }
