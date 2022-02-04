541ef4be811939343d00b68ef5357267c57e1d76
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2022-02-03 19:48:27 -0800

    fix: from toString to jsonEncode

--- evac_app/test/drill_event_json_test.dart
+++ evac_app/test/drill_event_json_test.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:evac_app/models/drill_event.dart';
 
 void main() {
   var drillEventJson = DrillEvent.example().toJson();
   var drillEvent = DrillEvent.fromJson(drillEventJson);
-  print(drillEvent.toJson().toString());
+  print(jsonEncode(drillEvent.toJson()));
 }
