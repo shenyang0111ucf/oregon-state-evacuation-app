7c5c121cd69b91004d6997c17f7eaa91be7b86ac
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2022-05-05 21:26:45 -0700

    Create upload_details.dart

---
+++ evac_app/lib/models/drill_details/drill_tasks/task_details/upload_details.dart
@@ -0,0 +1,7 @@
+import 'package:evac_app/models/drill_details/drill_tasks/task_details/task_details.dart';
+
+class UploadDetails extends TaskDetails {
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+}
